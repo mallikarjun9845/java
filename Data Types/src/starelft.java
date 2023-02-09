885152f6cafcaa97b0841b985ac8e19866ce14c3
mallikarjun <mallikarjun@LAPTOP-LE23424T>
2023-02-09 17:20:46 +0530

    First commit

---
+++ Data Types/src/starelft.java
@@ -0,0 +1,26 @@
+
+	public class starelft
+	{
+
+		public static void main(String[] args) 
+		{
+			// TODO Auto-generated method stub
+			int b =5;
+			for(int i=1;i<=b;i++)
+			{
+				
+				for(int x=1;x<=i;x++) 
+				{
+					System.out.print("@ ");
+				}
+				System.out.println();
+				
+				
+			}
+				
+
+		}
+
+	}
+
+
--- ProjectA12/src/FirstProgramm.java
+++ ProjectA12/src/FirstProgramm.java
@@ -1,6 +1,16 @@
-
+class Sample{
+	int a,b;
+	int c;
+	void show()
+	{
+		System.out.println("Values are "+a+" " +b ");
+				
+	}
+}
 public class FirstProgramm {
 	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.show();
 		int sum=10+20;
 		System.out.println("Sum is" +sum);
 	}
